268b7d0cb8273d2efaee9c60a7cbcd87f74d820b
zhangxiaolong <longxz321@163.com>
2018-12-02 16:18:06 +0800

    项部署脚本

--- deploy.sh
+++ deploy.sh
@@ -35,7 +35,7 @@ rm -f $TOMCAT_APP_PATH/webapps/order.war
 cp $PROJ_PATH/order/target/order.war $TOMCAT_APP_PATH/webapps/
 
 cd $TOMCAT_APP_PATH/webapps/
-sudo chmod -R order.war
+sudo chmod -R 777 order.war
 #mv order.war ROOT.war
 
 # 启动Tomcat
